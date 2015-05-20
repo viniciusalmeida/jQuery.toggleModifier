d87d07c048bb48f3c6cd44751812abd8a1bb55fe
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2015-05-19 23:41:45 -0300

    Add build taks to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,30 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    pkg: grunt.file.readJSON('bower.json'),
+
     qunit: {
       files: ['tests/index.html']
+    },
+
+    uglify: {
+      build: {
+        files: {
+          'dist/jQuery.toggleModifier.min.js': ['lib/jQuery.toggleModifier.js']
+        }
+      }
+    },
+
+    copy: {
+      main: {
+        src: 'lib/jQuery.toggleModifier.js',
+        dest: 'dist/jQuery.toggleModifier.js'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('test', 'qunit');
+  grunt.registerTask('build', ['uglify:build', 'copy']);
 };
